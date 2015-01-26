4a2718d4398b2d544b24b3466ea53cad0b785d1c
melissa-mccoy <melissa.l.mccoy@gmail.com>
2015-01-26 02:02:39 +0000

    fixed database and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
-require 'Date'
 
 #Fake Seeds
-begin
-fnames = "a,b,c,d".split(",")
-lnames = "A,B,C,D".split(",")
+# begin
+fnames = "Helen,b,c,d".split(",")
+lnames = "Baxendale,B,C,D".split(",")
 countries = "aa,bb,cc,dd".split(",")
-birthdays = [Date.new(1990,1,1),Date.new(1992,2,2),Date.new(1993,3,3),Date.new(1994,4,4)]
+birthdays = [Date.new(1990,1,26),Date.new(1992,2,2),Date.new(1993,3,3),Date.new(1994,4,4)]
 
-fnames.each_with_index { |fname,index|
-    Scholar.create(first_name: fname, last_name: lnames[index], country: countries[index] , birthday: birthdays[index], note: "Sale on water started saturday")
-}
-rescue ActiveRecord::RecordInvalid => invalid
-  puts invalid.record.errors
-end
+Scholar.create(birthdate: Date.new(1990,1,26))
+# fnames.each_with_index { |fname,index|
+#     p fname
+#     Scholar.create(birthdate: "bob")
+# }
+# rescue ActiveRecord::RecordInvalid => invalid
+#   puts invalid.record.errors
+# end
 
 #CSV Seeds
 
